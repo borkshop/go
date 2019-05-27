c5cccaee52300026490d2660903346360014e609
Joshua T Corbin <jcorbin@wunjo.org>
2019-05-26 21:22:34 -0700

    Drop unused polyfill

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// polyfill
-if (!WebAssembly.instantiateStreaming) {
-	WebAssembly.instantiateStreaming = async (resp, importObject) => {
-		const source = await (await resp).arrayBuffer();
-		return await WebAssembly.instantiate(source, importObject);
-	};
-}
-
 async function init() {
 	const statusEl = document.querySelector('#status');
 
