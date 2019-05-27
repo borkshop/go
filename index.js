6c9535340a2bc45d546f68834f7ab8d0ff241b9a
Joshua T Corbin <jcorbin@wunjo.org>
2019-05-26 21:29:34 -0700

    Reify go runner

--- index.js
+++ index.js
@@ -1,48 +1,68 @@
-async function init() {
-	const statusEl = document.querySelector('#status');
-
-	let resp = await fetch("build.json");
-	const buildInfo = await resp.json();
-	document.title += ': ' + buildInfo.Package.ImportPath;
-	statusEl.innerHTML = `Building <tt>${buildInfo.Package.ImportPath}</tt>...`;
-
-	resp = await fetch("main.wasm");
-
-	if (/^text\/plain($|;)/.test(resp.headers.get('Content-Type'))) {
-		statusEl.innerHTML = `<pre id="buildLog"></pre>`;
-		const log = document.querySelector('#buildLog');
-		log.innerText = await resp.text();
-		return;
+global.GoRunner = class {
+	constructor(opts) {
+		if (!opts) opts = {};
+		this.el = opts.el;
+		this.href = opts.href || 'build.json';
+		this.data = null;
+		this.module = null;
+		this.argv0 = 'wasm';
+		this.load();
 	}
 
-	const module = await WebAssembly.compileStreaming(resp);
+	async load() {
+		let resp = await fetch(this.href);
+		this.data = await resp.json();
+		document.title += ': ' + this.data.Package.ImportPath;
+		this.el.innerHTML = `Building <tt>${this.data.Package.ImportPath}</tt>...`;
 
-	statusEl.innerHTML = `<input id="argv" size="40" title="JSON-encoded ARGV" /><button id="run">Run</button>`;
-	const runButton = document.querySelector('#run');
-	const argvInput = document.querySelector('#argv');
+		const basename = this.data.Package.Dir.split('/').pop();
+		this.argv0 = basename + '.wasm';
 
-	const basename = buildInfo.Package.Dir.split('/').pop();
-	argvInput.value = JSON.stringify([basename + '.wasm']);
+		resp = await fetch("main.wasm");
+		if (/^text\/plain($|;)/.test(resp.headers.get('Content-Type'))) {
+			this.el.innerHTML = `<pre id="buildLog"></pre>`;
+			const log = document.querySelector('#buildLog');
+			log.innerText = await resp.text();
+			return;
+		}
+		this.module = await WebAssembly.compileStreaming(resp);
 
-	runButton.onclick = async function() {
-		const argv = JSON.parse(argvInput.value);
+		return this.interact();
+	}
 
-		runButton.disabled = true;
-		runButton.innerText = 'Running...';
+	async interact() {
+		this.el.innerHTML = `<input id="argv" size="40" title="JSON-encoded ARGV" /><button id="run">Run</button>`;
+		const runButton = document.querySelector('#run');
+		const argvInput = document.querySelector('#argv');
 
-		console.clear();
-		console.log('Running', buildInfo.Package, 'with args', argv);
+		argvInput.value = JSON.stringify([this.argv0]);
 
+		runButton.onclick = async () => {
+			const argv = JSON.parse(argvInput.value);
+
+			runButton.disabled = true;
+			runButton.innerText = 'Running...';
+			this.el.style.display = 'none';
+
+			console.clear();
+			await this.run(argv);
+
+			this.el.style.display = '';
+			runButton.innerText = 'Run';
+			runButton.disabled = false;
+		};
+	}
+
+	async run(argv) {
 		const go = new Go();
 		go.argv = argv;
-		const instance = await WebAssembly.instantiate(module, go.importObject);
-		statusEl.style.display = 'none';
+		const instance = await WebAssembly.instantiate(this.module, go.importObject);
 		await go.run(instance);
+	}
+};
 
-		statusEl.style.display = '';
-		runButton.innerText = 'Run';
-		runButton.disabled = false;
-	};
-}
+(() => {
+	const el = document.querySelector('#status');
 
-init();
+	global.goRun = new GoRunner({el});
+})();
