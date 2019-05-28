88e86ab3c877375722a783f5b6b3554a4ceb2e0d
Joshua T Corbin <jcorbin@wunjo.org>
2019-05-27 21:41:21 -0700

    Anonymize the hello callback

--- handler/example_hello_test.go
+++ handler/example_hello_test.go
@@ -5,14 +5,13 @@ package handler_test
 import "syscall/js"
 
 var (
-	window = js.Global().Get("window")
+	window   = js.Global().Get("window")
 	document = js.Global().Get("document")
 )
 
 func Example_hello() {
-	window.Call("addEventListener", "DOMContentLoaded", js.FuncOf(contentLoaded))
-}
-
-func contentLoaded(this js.Value, args []js.Value) interface{} {
-	document.Get("body").Set("innerText", "hello world")
+	window.Call("addEventListener", "DOMContentLoaded",
+		js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			document.Get("body").Set("innerText", "hello world")
+		}))
 }
