3f9082a70ff8b4ba562a947953701f9adbc4c391
Joshua T Corbin <jcorbin@wunjo.org>
2019-06-02 12:38:07 -0700

    Re-generate automaton server

--- src/borkshop/automaton/server.go
+++ src/borkshop/automaton/server.go
@@ -1,69 +1,46 @@
 // +build !js
 
+//go:generate go run github.com/jcorbin/gorunwasm -gen borkshop/automaton
+
 package main
 
 import (
 	"flag"
 	"fmt"
-	"go/build"
 	"log"
 	"net"
 	"net/http"
-	"os"
-	"path/filepath"
 
 	"github.com/jcorbin/gorunwasm/handler"
 )
 
-var listen = "localhost:8080"
+var (
+	listen = "localhost:0"
+
+	srcDir = ""
+	path   = "borkshop/automaton"
+)
 
 func main() {
-	flag.StringVar(&listen, "listen", listen, "http listen address")
+	flag.StringVar(&listen, "listen", listen, "listen address for http server")
 	flag.Parse()
-	if err := run(); err != nil {
-		log.Fatalln(err)
-	}
+	log.Fatalln(serve())
 }
 
-func run() error {
-	wd, err := os.Getwd()
+func serve() error {
+	wh, err := handler.Handle("", srcDir, path)
 	if err != nil {
 		return err
 	}
-
-	pkg, err := build.Default.Import("borkshop/automaton", wd, 0)
-	if err != nil {
-		return err
-	}
-	srcDir := pkg.Dir
-
-	// TODO better access to the gorunwasm runner script
-	gorunwasm, err := build.Default.Import("github.com/jcorbin/gorunwasm", wd, 0)
-	if err != nil {
-		return err
-	}
-	http.Handle("/index.js", serveFile(filepath.Join(gorunwasm.Dir, "index.js")))
-
-	http.Handle("/", http.FileServer(http.Dir(srcDir)))
-	wh, err := handler.NewWASMHandler(srcDir, ".")
-	if err != nil {
-		return err
-	}
-
-	http.Handle("/main.wasm", wh)
-	http.Handle("/wasm_exec.js", serveFile(wh.WASMExec()))
+	defer wh.Close()
 
 	ln, err := net.Listen("tcp", listen)
 	if err != nil {
 		return fmt.Errorf("listen %q failed: %v", listen, err)
 	}
 
-	log.Printf("Serving %q on http://%s", srcDir, listen)
+	log.Printf("listening on http://%v", ln.Addr())
+	log.Printf("Serving %v on http://%s", wh, ln.Addr())
+
 	return http.Serve(ln, nil)
-}
-
-type serveFile string
-
-func (sf serveFile) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	http.ServeFile(w, req, string(sf))
 }
